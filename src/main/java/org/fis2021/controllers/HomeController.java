c7dfb1b1a2a48df122b16b138b2a2591a495a9b7
Mihai <mihai1309f@gmail.com>
2021-04-18 14:05:02 +0300

    FA-3 Created GUI for the createThread page

--- src/main/java/org/fis2021/controllers/HomeController.java
+++ src/main/java/org/fis2021/controllers/HomeController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -22,11 +23,15 @@ public class HomeController{
     @FXML
     private BorderPane borderPane;
 
+    @FXML
+    private Label placeholderText;
+
     private User user;
 
     public void setUser(User u){
         user = u;
-        System.out.println(user);
+        placeholderText.setText(String.format("You are logged in as user %s and your role is %s!", u.getUsername(), u.getRole()));
+
     }
 
     private User getUser(){
