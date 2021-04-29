9b1eeb27f57671d4d474c448bd87379b272e5bac
Mihai <mihai1309f@gmail.com>
2021-04-29 12:28:47 +0300

    FA-6 added context menu to threads list on profile page

--- src/main/java/org/fis2021/controllers/DisplayProfileController.java
+++ src/main/java/org/fis2021/controllers/DisplayProfileController.java
@@ -1,12 +1,17 @@
 package org.fis2021.controllers;
 
 
+import com.sun.javafx.scene.control.ContextMenuContent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.fis2021.models.ForumThread;
@@ -37,6 +42,13 @@ public class DisplayProfileController {
 
     public String getDisplayedUsername() {
         return displayedUsername;
+    }
+
+    public ContextMenu menu;
+
+    public void initialize(){
+        ContextMenu menu = new ContextMenu();
+        threads.setContextMenu(menu);
     }
 
     public void setDisplayedUsername(String displayedUser) {
@@ -88,9 +100,23 @@ public class DisplayProfileController {
     }
 
     @FXML
-    public void handleListSelectAction(){
+    public void handleListSelectAction(MouseEvent event){
         try {
-            loadDisplayThreadPage(threadsList.get(threads.getSelectionModel().getSelectedIndex()).getTitle());
+            if(event.getButton() == MouseButton.PRIMARY){
+                loadDisplayThreadPage(threadsList.get(threads.getSelectionModel().getSelectedIndex()).getTitle());
+            }
+            else if(event.getButton() == MouseButton.SECONDARY && user.getRole().equals("Moderator")) {
+                threads.getContextMenu().getItems().clear();
+                if (threads.getSelectionModel().getSelectedIndex() >= 0) {
+                    threads.getContextMenu().getItems().add(new MenuItem("close thread"));
+                    threads.getContextMenu().getItems().get(0).setOnAction(
+                            (x) -> {
+                                threadsList.get(threads.getSelectionModel().getSelectedIndex()).setClosed(true);
+                                ThreadService.updateThread(threadsList.get(threads.getSelectionModel().getSelectedIndex()));
+                            }
+                    );
+                }
+            }
         }catch(IndexOutOfBoundsException e){
             return;
         }
